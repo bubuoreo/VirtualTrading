ba40a9abb65709beefd6e50b16466d9a2346ede2
TomDA13 <tom.darold@orange.fr>
2024-01-10 11:02:29 +0100

    crypto info is done

--- react-app/src/pages/CryptoDetailsPage.jsx
+++ react-app/src/pages/CryptoDetailsPage.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import CryptoItem from '../components/Crypto/CryptoItem';
 import CryptoCourbe from '../components/Crypto/CryptoCourbe';
+import CryptoInfo from '../components/Crypto/CryptoInfo';
 
 const CryptoDetailsPage = () => {
-    // Utilisez useParams pour récupérer les paramètres de l'URL
-    const { cryptoSymbol } = useParams();
+  const { cryptoSymbol } = useParams();
 
-    return (
-        <div className="bg-gray-200 min-h-screen">
-            <div className="container mx-auto">
-                <div> <CryptoCourbe cryptoSymbol={cryptoSymbol} /> </div>
-                <h1 className="text-4xl font-bold mt-8 mb-4">Crypto Details</h1>
-                <CryptoItem />
-            </div>
+  return (
+    <div className="bg-gray-200 min-h-screen">
+      <div className="container mx-auto">
+        <div>
+          <CryptoCourbe cryptoSymbol={cryptoSymbol} />
         </div>
-    );
+        <div>
+          <CryptoInfo cryptoSymbol={cryptoSymbol} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CryptoDetailsPage;
