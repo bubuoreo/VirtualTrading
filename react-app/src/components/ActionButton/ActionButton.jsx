8905ca29db71225c746eeb892152fb534f213eff
TomDA13 <tom.darold@orange.fr>
2024-01-11 08:42:53 +0100

    button added

--- react-app/src/components/ActionButton/ActionButton.jsx
+++ react-app/src/components/ActionButton/ActionButton.jsx
@@ -1,30 +1,16 @@
 import React from 'react';
-import { FaShoppingCart, FaDollarSign, FaPlay } from 'react-icons/fa'; // Install react-icons if not already
-import { useNavigate } from 'react-router-dom';
+import { Button } from '@chakra-ui/react';
 
-const icons = {
-  buy: FaShoppingCart,
-  sell: FaDollarSign,
-  selection: FaPlay,
-  play: FaPlay
-};
+const ActionButton = ({ type }) => {
+    const isBuyAction = type === 'buy';
+    const buttonText = isBuyAction ? 'Buy' : 'Sell';
+    const buttonColor = isBuyAction ? 'green' : 'red';
 
-const ActionButton = ({ action, label }) => {
-  const IconComponent = icons[action];
-
-  const navigate = useNavigate();
-
-  const handleAction = () => {
-    // Directly navigate to the home page without credentials check
-    navigate('/'+action);}
-
-  return (
-    <button onClick={handleAction} className="px-4 py-4 border rounded shadow m-auto justify-center">
-      {IconComponent ? <IconComponent size={30} /> : null}
-      {label}
-    </button>
-  );
+    return (
+        <Button colorScheme={buttonColor} size="sm">
+            {buttonText}
+        </Button>
+    );
 };
 
 export default ActionButton;
-
